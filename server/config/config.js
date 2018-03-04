795f64784ea3afebf2ab32754b779c1887a99a20
imkartikahuja <kartik.ahuja24@gmail.com>
2018-03-04 14:45:23 +0530

    Update congif to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,13 +1,11 @@
 var env = process.env.NODE_ENV || 'development';
 console.log('env ****', env);
 
-var REMOTE_MONGO = 'mongodb://todoapp:imkartikahuja@ds249718.mlab.com:49718/todo-app';
-var LOCAL_MONGO = 'mongodb://localhost:27017/TodoApp';
- process.env.MONGODB_URI = process.env.PORT ? REMOTE_MONGO : LOCAL_MONGO;
+if (env === 'development' || env === 'test') {
+  var config = require('./config.json');  //automatically converts json to JS object
+  var envConfig = config[env];
 
-if(env === 'development'){
-  process.env.PORT = 3000;
-} else if (env === 'test') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+  Object.keys(envConfig).forEach((key) => {
+    process.env[key] = envConfig[key];
+  });
 }
--- server/models/user.js
+++ server/models/user.js
@@ -43,7 +43,7 @@ UserSchema.methods.toJSON = function () {     //to return only id n email else i
 UserSchema.methods.generateAuthToken = function () {
   var user = this;      //now we have access to doc which it is called on
   var access = 'auth';
-  var token = jwt.sign({_id: user._id.toHexString(),access}, 'abc123').toString();
+  var token = jwt.sign({_id: user._id.toHexString(),access}, process.env.JWT_SECRET).toString();
 
   // user.tokens.push({access,token});
   user.tokens = user.tokens.concat([{access,token}]);
@@ -70,7 +70,7 @@ UserSchema.statics.findByToken = function (token) {       //model method
   var decoded;
 
   try {
-      decoded = jwt.verify(token,'abc123');
+      decoded = jwt.verify(token,process.env.JWT_SECRET);
   } catch (e) {
       // return new Promise(function(resolve, reject) {
       //   reject();
